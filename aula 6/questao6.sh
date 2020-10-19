c13de73fd49e8daedd979fbb2dcfde890cc5959d
diego98martins <diego98martins@gmail.com>
2020-10-19 20:50:09 -0300

    Update questao6.sh

--- aula 6/questao6.sh
+++ aula 6/questao6.sh
@@ -4,7 +4,8 @@ ls=$(ls)
 
 for x in *; do
 	for y in *; do
-		if [ $x -ef $y ]; then
+		# verifica se x e y são arquivos, se x e y são arquivos diferentes, por fim o número de linhas
+		if [ -f $x -a -f $y -a $x != $y -a $(cat $x | wc -l) -eq $(cat $y | wc -l) ]; then
 			echo "$x e $y tem a mesma quantidade de linhas"
 		fi
 	done
